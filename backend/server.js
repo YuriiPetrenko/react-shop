f63066f31fc95b9e6de32894a36a9d9e13bb0aff
yuriipetrenko <obzora6@gmail.com>
2021-05-04 17:42:37 +0200

    add user controller and user routes

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,11 @@ import connectDB from './config/db.js'
 import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 const app = express()
+
+app.use(express.json())
 
 dotenv.config()
 
@@ -16,6 +19,7 @@ app.get('/', (req, res)=>{
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
